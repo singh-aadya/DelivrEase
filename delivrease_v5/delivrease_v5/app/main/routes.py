5f3ccf3eb5d5d7abe5508db0308ce7e1c7519a95
AADYA SINGH <152200033+singh-aadya@users.noreply.github.com>
2025-11-05 21:45:41 +0530

    Delete delivrease_v5/delivrease_v5/app directory

--- delivrease_v5/delivrease_v5/app/main/routes.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, render_template
-main_bp = Blueprint("main", __name__, template_folder="templates")
-@main_bp.route("/")
-def index():
-    return render_template("main/index.html")
